5071555553ecef5278d3b02aa2e66617f595ac2d
tdlrhg <tendler.an@gmail.com>
2026-04-14 23:42:28 +0200

    Add files via upload

--- prozente/aufgaben/p_1_1.js
+++ prozente/aufgaben/p_1_1.js
@@ -1,13 +1,13 @@
 var aufgabenPool_p_1_1 = [
   {
     format: 'mc',
-    kontext: '<span style="font-size:1.4rem"><span class="bruch"><span class="z">3</span><span class="n">4</span></span></span>',
+    kontext: '<span style="font-size:1.4rem"><span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">3</span><span style="display:block;padding:1px 3px 0">4</span></span></span>',
     frage: 'Welche Darstellungen entsprechen alle diesem Bruch?',
     optionen: [
-      '<span class="bruch"><span class="z">75</span><span class="n">100</span></span> &nbsp;·&nbsp; 0,75 &nbsp;·&nbsp; 75%',
-      '<span class="bruch"><span class="z">34</span><span class="n">100</span></span> &nbsp;·&nbsp; 0,34 &nbsp;·&nbsp; 34%',
-      '<span class="bruch"><span class="z">75</span><span class="n">100</span></span> &nbsp;·&nbsp; 7,5 &nbsp;·&nbsp; 75%',
-      '<span class="bruch"><span class="z">3</span><span class="n">100</span></span> &nbsp;·&nbsp; 0,03 &nbsp;·&nbsp; 3%'
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">75</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 0,75 &nbsp;·&nbsp; 75%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">34</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 0,34 &nbsp;·&nbsp; 34%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">75</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 7,5 &nbsp;·&nbsp; 75%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">3</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 0,03 &nbsp;·&nbsp; 3%'
     ],
     richtig: 0,
     erklaerung: '3/4 = 75/100 = 0,75 = 75%'
@@ -17,10 +17,10 @@ var aufgabenPool_p_1_1 = [
     kontext: '0,6',
     frage: 'Welche Darstellungen entsprechen alle dieser Dezimalzahl?',
     optionen: [
-      '<span class="bruch"><span class="z">3</span><span class="n">5</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">60</span><span class="n">100</span></span> &nbsp;·&nbsp; 60%',
-      '<span class="bruch"><span class="z">6</span><span class="n">5</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">60</span><span class="n">100</span></span> &nbsp;·&nbsp; 60%',
-      '<span class="bruch"><span class="z">3</span><span class="n">5</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">6</span><span class="n">100</span></span> &nbsp;·&nbsp; 6%',
-      '<span class="bruch"><span class="z">6</span><span class="n">1</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">60</span><span class="n">100</span></span> &nbsp;·&nbsp; 6%'
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">3</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">60</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 60%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">6</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">60</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 60%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">3</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">6</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 6%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">6</span><span style="display:block;padding:1px 3px 0">1</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">60</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; 6%'
     ],
     richtig: 0,
     erklaerung: '0,6 = 6/10 = 3/5 = 60/100 = 60%'
@@ -30,17 +30,17 @@ var aufgabenPool_p_1_1 = [
     kontext: '40%',
     frage: 'Welche Darstellungen entsprechen alle dieser Prozentzahl?',
     optionen: [
-      '<span class="bruch"><span class="z">40</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">5</span></span> &nbsp;·&nbsp; 0,4',
-      '<span class="bruch"><span class="z">40</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">4</span><span class="n">5</span></span> &nbsp;·&nbsp; 0,4',
-      '<span class="bruch"><span class="z">40</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">5</span></span> &nbsp;·&nbsp; 0,04',
-      '<span class="bruch"><span class="z">4</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">5</span></span> &nbsp;·&nbsp; 0,4'
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">40</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; 0,4',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">40</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">4</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; 0,4',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">40</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; 0,04',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">4</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">5</span></span> &nbsp;·&nbsp; 0,4'
     ],
     richtig: 0,
     erklaerung: '40% = 40/100 = 2/5 = 0,4'
   },
   {
     format: 'mc',
-    kontext: '<span style="font-size:1.4rem"><span class="bruch"><span class="z">15</span><span class="n">60</span></span></span>',
+    kontext: '<span style="font-size:1.4rem"><span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">15</span><span style="display:block;padding:1px 3px 0">60</span></span></span>',
     frage: 'Welche Prozentzahl entspricht diesem Bruch?',
     optionen: ['25%', '15%', '60%', '4%'],
     richtig: 0,
@@ -51,10 +51,10 @@ var aufgabenPool_p_1_1 = [
     kontext: '0,08',
     frage: 'Welche Darstellungen entsprechen alle dieser Dezimalzahl?',
     optionen: [
-      '<span class="bruch"><span class="z">8</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">25</span></span> &nbsp;·&nbsp; 8%',
-      '<span class="bruch"><span class="z">8</span><span class="n">10</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">25</span></span> &nbsp;·&nbsp; 80%',
-      '<span class="bruch"><span class="z">8</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">4</span><span class="n">25</span></span> &nbsp;·&nbsp; 8%',
-      '<span class="bruch"><span class="z">8</span><span class="n">100</span></span> &nbsp;·&nbsp; <span class="bruch"><span class="z">2</span><span class="n">25</span></span> &nbsp;·&nbsp; 0,8%'
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">8</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">25</span></span> &nbsp;·&nbsp; 8%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">8</span><span style="display:block;padding:1px 3px 0">10</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">25</span></span> &nbsp;·&nbsp; 80%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">8</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">4</span><span style="display:block;padding:1px 3px 0">25</span></span> &nbsp;·&nbsp; 8%',
+      '<span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">8</span><span style="display:block;padding:1px 3px 0">100</span></span> &nbsp;·&nbsp; <span style="display:inline-block;text-align:center;vertical-align:middle;line-height:1.1"><span style="display:block;border-bottom:1.5px solid currentColor;padding:0 3px 1px">2</span><span style="display:block;padding:1px 3px 0">25</span></span> &nbsp;·&nbsp; 0,8%'
     ],
     richtig: 0,
     erklaerung: '0,08 = 8/100 = 2/25 = 8%'
